2617942cb63f0becd5a93b125f6357b0a1bf90d4
Leonardo Paz <leopaz08@gmail.com>
2025-03-19 19:53:48 -0500

    cambios de imagen

--- correo.js
+++ correo.js
@@ -3,22 +3,10 @@ const logo = document.getElementById("company-logo");
 const whatsappLink = document.getElementById("whatsapp-link");
 
 // Cargar imagen desde Postimg
-logo.src = "https://i.postimg.cc/25KRD2Nf/logo-HEADER.gif";
+logo.src = "logo-HEADER.gif";
 
 // Configurar enlace de WhatsApp
 whatsappLink.href =
   "https://wa.me/51924796779?text=Hola,%20quiero%20agendar%20una%20reunión.";
 
-  // https://i.postimg.cc/G2T5FR84/facebook.png
-  //https://i.postimg.cc/WzMYy77j/whatsap.png
-  /*
-  [url=https://postimg.cc/vxtpMrnq][img]https://i.postimg.cc/vxtpMrnq/book.png[/img][/url]
-
-[url=https://postimg.cc/XXKMwbPf][img]https://i.postimg.cc/XXKMwbPf/imagws.png[/img][/url]
-
-[url=https://postimages.org/][img]
-https://i.postimg.cc/q6T99bN6/imgtoc.png[/img][/url]
-https://i.postimg.cc/RC7zy1hN/milike.png
-  https://i.postimg.cc/Dy53y2qh/www.png
-  https://i.postimg.cc/HsN3tRm5/img-Instala.jpg
-  */
+  
